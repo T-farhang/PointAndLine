18e709bd4db5dcd4983e2afc251b2741cd7a9975
T-farhang <tahereh.farhang@gmail.com>
2018-05-31 00:40:53 +0430

    Revert "Revert "Point And line project""
    This reverts commit c63b49744f215cf3219fbbdf3b29ed190923d880.

--- src/pointandline/PointAndLine.java
+++ src/pointandline/PointAndLine.java
@@ -3,43 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package noghteandkhat;
+package pointandline;
 
 /**
  *
  * @author Totia
  */
-public class NoghteAndKhat {
+public class PointAndLine {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Noghte n1 = new Noghte(1,2);
+        Point n1 = new Point(1, 2);
         System.out.println(n1);
-        
-        Noghte n2 = new Noghte();
+
+        Point n2 = new Point();
         System.out.println(n2);
-        
+
         n1.setxPosition(3);
         n1.setyPosition(4);
         System.out.println(n1);
-        System.out.println("X is : " +n1.getxPosition());
-        System.out.println("Y is : " +n1.getyPosition());
-        
+        System.out.println("X is : " + n1.getxPosition());
+        System.out.println("Y is : " + n1.getyPosition());
+
         n1.setXY(5, 6);
-        System.out.println("X is : "+n1.getXY()[0]);
-        System.out.println("Y is : "+n1.getXY()[1]);
-        
+        System.out.println("X is : " + n1.getXY()[0]);
+        System.out.println("Y is : " + n1.getXY()[1]);
+
         n2.setXY(1, 1);
         System.out.printf("distance is : %.2f%n", n2.distance(3, 3));
         System.out.printf("distance is : %.2f%n", n1.distance(n2));
         System.out.printf("distance is : %.2f%n", n2.distance(n1));
         System.out.printf("distance is : %.2f%n", n1.distance());
         System.out.printf("distance is : %.2f%n", n2.distance());
-        
-        System.out.printf("Shib : %.2f%n" , n2.Shib(2, 2));
-      
+
+        System.out.printf("Slope : %.2f%n", n2.Slope(2, 2));
+
     }
 
 }
